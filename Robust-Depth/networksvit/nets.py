5838aaa7dd87bba98d9579d67738ed74b6e5da1b
Kieran <51883968+kieran514@users.noreply.github.com>
2023-06-06 11:38:40 +0100

    Update nets.py

--- Robust-Depth/networksvit/nets.py
+++ Robust-Depth/networksvit/nets.py
@@ -1,16 +1,12 @@
 from __future__ import absolute_import, division, print_function
-#from msilib.schema import Class
 
 import numpy as np
 import torch
 import torch.nn as nn
 
 from collections import OrderedDict
-#from layers import *
 
-#from .resnet_encoder import ResnetEncoder
 from .hr_decoder import DepthDecoder
-#from .pose_decoder import PoseDecoder
 from .mpvit import *
 
 
@@ -22,16 +18,13 @@ class DeepNet(nn.Module):
         self.weights_init=weights_init
         self.num_pose_frames=num_pose_frames
         self.scales = scales
-
-
         if self.type =='mpvitnet':
             self.encoder = mpvit_small()
             self.decoder = DepthDecoder()
  
         else:
             print("wrong type of the networks, only depthnet and posenet")
-    
-
+            
     def forward(self, inputs):
         if self.type =='mpvitnet': 
             self.outputs = self.decoder(self.encoder(inputs))
