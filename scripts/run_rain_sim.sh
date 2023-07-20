8aef707b7ef14c6342b3d303857cef3a993d1959
Kieran <51883968+kieran514@users.noreply.github.com>
2023-07-20 12:54:00 +0100

    Add files via upload

---
+++ scripts/run_rain_sim.sh
@@ -0,0 +1,254 @@
+
+cd pytorch-CycleGAN-and-pix2pix-master
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0028_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0028_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0070_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0070_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0001_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0001_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0002_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0002_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0005_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0005_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0009_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0009_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0011_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0011_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0013_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0013_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0014_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0014_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0015_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0015_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0017_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0017_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0018_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0018_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0019_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0019_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0020_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0020_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0022_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0022_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0023_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0023_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0027_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0027_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0029_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0029_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0032_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0032_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0035_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0035_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0036_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0036_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0039_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0039_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0046_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0046_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0048_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0048_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0051_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0051_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0052_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0052_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0056_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0056_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0057_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0057_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0059_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0059_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0060_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0060_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0061_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0061_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0064_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0064_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0079_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0079_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0084_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0084_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0086_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0086_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0087_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0087_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0091_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0091_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0093_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0093_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0095_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0095_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0096_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0096_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0101_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0101_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0104_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0104_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0106_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0106_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0113_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0113_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0117_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_26/2011_09_26_drive_0117_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_29/2011_09_29_drive_0004_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_29/2011_09_29_drive_0004_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_29/2011_09_29_drive_0026_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_29/2011_09_29_drive_0026_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_29/2011_09_29_drive_0071_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_29/2011_09_29_drive_0071_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_30/2011_09_30_drive_0016_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_30/2011_09_30_drive_0016_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_30/2011_09_30_drive_0018_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_30/2011_09_30_drive_0018_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_30/2011_09_30_drive_0020_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_30/2011_09_30_drive_0020_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_30/2011_09_30_drive_0027_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_30/2011_09_30_drive_0027_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_30/2011_09_30_drive_0028_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_30/2011_09_30_drive_0028_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_30/2011_09_30_drive_0033_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_30/2011_09_30_drive_0033_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_30/2011_09_30_drive_0034_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_30/2011_09_30_drive_0034_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_10_03/2011_10_03_drive_0042_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_10_03/2011_10_03_drive_0042_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_10_03/2011_10_03_drive_0047_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_10_03/2011_10_03_drive_0047_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_28/2011_09_28_drive_0001_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_28/2011_09_28_drive_0001_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_09_28/2011_09_28_drive_0002_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_09_28/2011_09_28_drive_0002_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_10_03/2011_10_03_drive_0034_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_10_03/2011_10_03_drive_0034_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
+
+python test.py \
+--dataroot ../data/KITTI_RAW/2011_10_03/2011_10_03_drive_0027_sync/image_02/data \
+--results_dir ../data/KITTI_RAW/2011_10_03/2011_10_03_drive_0027_sync/image_02/rain_gan/ --name rain_cyclegan --model test --no_dropout --preprocess none
